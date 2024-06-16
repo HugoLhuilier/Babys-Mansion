a1792e2c7b9028dcffae101707007e95f188a5e0
Hugo <115175312+HugoLhuilier@users.noreply.github.com>
2024-06-16 23:13:02 +0200

    CameraController full implementation
    CameraController component controls the view to follow the given player

--- src/Components/CameraController.cpp
+++ src/Components/CameraController.cpp
@@ -1,6 +1,7 @@
 #include "Object.h"
 #include "Game.h"
 #include "CameraController.h"
+#include <iostream>
 
 using namespace std;
 
@@ -8,10 +9,24 @@ void CameraController::update()
 {
 	if (player) {
 		sf::View nView = win->getView();
-		nView.setCenter(player->getPos());
+		sf::Vector2f playPos = player->getPos();
+		sf::Vector2f nPos(clamp(playPos.x, lowerBound.x, upperBound.x), clamp(playPos.y, lowerBound.y, upperBound.y));
+		nView.setCenter(nPos);
+		win->setView(nView);
 	}
 }
 
 void CameraController::init() {
 	win = object->getGame()->getWindow();
+}
+
+void CameraController::setPlayer(Object* nP)
+{
+	player = nP;
+}
+
+void CameraController::setBounds(sf::Vector2f lower, sf::Vector2f upper)
+{
+	lowerBound = lower;
+	upperBound = upper;
 }
